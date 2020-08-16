180d2bc9c7edec788d1271a665bf13ed54bd9ac7
Arkkarachat  Siribout <arkkarachat.s@gmail.com>
2020-08-16 19:46:33 +0700

    pipe-custom

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-my-pipe-custom';
+  dummy = { name: 'Arkkarachat', password:'1234'}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormatFilesSizePipe } from './format-files-size.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormatFilesSizePipe
   ],
   imports: [
     BrowserModule,
