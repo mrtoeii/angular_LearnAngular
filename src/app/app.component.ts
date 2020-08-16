239bf670d79de8b8df91fb20a130758d1ff12eda
Arkkarachat  Siribout <arkkarachat.s@gmail.com>
2020-08-16 20:05:22 +0700

    ngComponentsComu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Com1Component } from './components/com1/com1.component';
+import { Com2Component } from './components/com2/com2.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ngComponentsComu';
+  title = 'Angular Component Commucation';
+  count = 0
+
+  @ViewChild("c1") c1: Com1Component 
+  @ViewChild("c2") c2: Com2Component 
+
+  onClickCount(){
+    this.count += 1
+  }
+
+  onCom1Rest(value){
+    this.count = value
+  }
+
+  onClickRun3(){
+    this.c1.count = 99
+    this.c2.count = 11
+  }
 }
